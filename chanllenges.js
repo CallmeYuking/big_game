ef167a1c79a6f8a88f2ec2d3a09ce91394309ea7
vagrant <vagrant@vagrant.vm>
2019-12-15 08:54:08 +0000

    fixed all the new feature.

--- chanllenges.js
+++ chanllenges.js
@@ -33,7 +33,8 @@ function init() {
     activePlayer = 0;
     gamePlaying = true;
 
-    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('#dice-1').style.display = 'none';
+    document.querySelector('#dice-2').style.display = 'none';
 
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
@@ -66,35 +67,30 @@ function nextPlayer() {
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
 
-    document.querySelector('.dice').style.display = 'none'
+    document.querySelector('#dice-1').style.display = 'none'
+    document.querySelector('#dice-2').style.display = 'none'
 };
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     if(gamePlaying) {
         // 1, Random number
-        var dice = Math.floor(Math.random() * 6) + 1;
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
 
         // 2, Display the result
-        var diceDom = document.querySelector('.dice');
-        diceDom.style.display = 'block';
-        diceDom.src = `dice-${dice}.png`;
+        document.getElementById('dice-1').style.display = 'block';
+        document.getElementById('dice-2').style.display = 'block'; 
+        document.getElementById('dice-1').src = `dice-${dice1}.png`;
+        document.getElementById('dice-2').src = `dice-${dice2}.png`;
 
         // 3, Update the round score IF the rolled number was Not a 1
-        if(dice === 6 && lastDice === 6) {
-            //player looses score
-            scores[activePlayer] = 0;
-            document.querySelector(`#score-${activePlayer}`).textContent = '0';
-            nextPlayer();
-        }else if(dice !== 1) {
-            // add score
-            roundScore += dice;
+        if(dice1 !== 1 && dice2 !== 1) { 
+            roundScore += dice1 + dice2;
             document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
         }else{
             //next player
             nextPlayer();
         }
-
-        lastDice = dice;
     }
 });
 
@@ -119,7 +115,8 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         // check if player won the game
         if (scores[activePlayer] >= winningScore) {
             document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!'
-            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('#dice-1').style.display = 'none';
+            document.querySelector('#dice-2').style.display = 'none';
             document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
             document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
             gamePlaying = false;
