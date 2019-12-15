ae499e2eee57b2f34f25eb68615ad55c0d5244f3
vagrant <vagrant@vagrant.vm>
2019-12-15 06:39:57 +0000

    finish pig game by adding 3 eventListener including btn-roll/btn-hold/new-game)

--- app.js
+++ app.js
@@ -8,26 +8,92 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer;
-scores = [0,0];
-roundScore = 0;
-activePlayer = 1;
+var scores, roundScore, activePlayer, gamePlaying;
 
-//document.querySelector('#current-' + activePlayer).textContent = dice;
+init();
 
-document.querySelector('.dice').style.display = 'none';
+function init() {
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+    gamePlaying = true;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+};
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; 
+    // if(activePlager === 0) {
+    //     activePlayer = 1;
+    // }else{
+    //     activePlayer = 0;
+    // }
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active')
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none'
+};
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
+    if(gamePlaying) {
+        // 1, Random number
+        var dice = Math.floor(Math.random() * 6) + 1;
 
-    // 1, Random number
-    var dice = Math.floor(Math.random() * 6) + 1;
+        // 2, Display the result
+        var diceDom = document.querySelector('.dice');
+        diceDom.style.display = 'block';
+        diceDom.src = `dice-${dice}.png`;
 
-    // 2, Display the result
-    var diceDom = document.querySelector('.dice');
-    diceDom.style.display = 'block';
-    diceDom.src = `dice-${dice}.png`;
-    //'dice-' + dice + '.png';
+        // 3, Update the round score IF the rolled number was Not a 1
+        if(dice !== 1) {
+            // add score
+            roundScore += dice;
+            document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+        }else{
+            //next player
+            nextPlayer();
+        }
+    }
+});
 
-    // 3, Update the round score IF the rolled number was Not a 1
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    if(gamePlaying) {
+        // add currrent score to globel score
+        scores[activePlayer] += roundScore;
+        // update the UI
+        document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer];
+        // check if player won the game
+        if (scores[activePlayer] >= 20) {
+            document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!'
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
+            document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
+            gamePlaying = false;
+        }else{
+            // next player
+            nextPlayer();
+        }
+    } 
+});
 
-})
+document.querySelector('.btn-new').addEventListener('click', init);
